3cd205cdaa1d1024db002b905e99687a36324ed9
anucha-liefcapital <anucha@liefcapital.com>
2025-07-15 16:59:20 +0700

    add: swagger

--- src/users/dto/register.dto.ts
+++ src/users/dto/register.dto.ts
@@ -1,17 +1,29 @@
 import { IsEmail, IsString, MinLength, IsNotEmpty } from 'class-validator';
-
+import { ApiProperty } from '@nestjs/swagger';
 export class RegisterDto {
+  @ApiProperty({
+    description: 'Email for registration',
+  })
   @IsEmail()
   email: string;
 
+  @ApiProperty({
+    description: 'Password for registration',
+  })
   @IsString()
   @MinLength(6)
   password: string;
 
+  @ApiProperty({
+    description: 'Your name',
+  })
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
+  @ApiProperty({
+    description: 'Your lastname',
+  })
   @IsString()
   @IsNotEmpty()
   lastName: string;
